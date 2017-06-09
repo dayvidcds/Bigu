4e46ca615e5c8624265dcc7c6574cfc91e6e62d3
dayvidcds <dayvid.clementino@gmail.com>
2017-06-09 18:10:27 -0300

    desenvolvimento da classe de persistencia para as rotas
    Summary: adicionei algumas classes de persistência para nosso sistema
    
    Reviewers: sostenesg7
    
    Reviewed By: sostenesg7
    
    Subscribers: wedsonxse, sostenesg7, thiago-farias
    
    Maniphest Tasks: T25
    
    Differential Revision: https://phab.upelabs.com/D14

--- persistence/teste.js
+++ persistence/teste.js
@@ -1,22 +1,21 @@
 var con = require('./ConexaoDB')
-var RepositorioUsuarios = require('./RepositorioUsuarios');
+var RouteRepository = require('./RouteRepository');
+
+var user1 = { cpf: "1234", nome: "Sostenes Silva" };
+
+var userRoute1 = { 
+    user1,
+    origin: 'cha grande, pe',
+    destiny: 'gravata, pe'
+ };
 
 (async() => {
     try {
-        var rep = new RepositorioUsuarios(con);
+        var repRot = new RouteRepository(con);
+        await repRot.insertRoute(userRoute1);
+        //repRot.remove('5930d8365280ae3e58f78d32');
+        //console.log('listando rotas...' + await repRot.findRoutes());
 
-        await rep.inserir({ cpf: "1234", nome: "Thiago" });
-        await rep.inserir({ cpf: "4321", nome: "Sostenes" });
-
-        console.log('listando...' + await rep.listarTodos());
-
-        //console.log(JSON.stringify(rep.listarTodos()));
-
-        //await rep.remover("4321");
-
-        //console.log(JSON.stringify(rep.listarTodos()));
-
-        console.log("Fim.");
     } catch (err) {
         console.log("Erro: " + err);
     }
